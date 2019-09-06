7ff0abeb0decfaea6af23dd030eff8ac4a787a4e
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-06 12:16:24 +0100

    created a MinimumLength component that contains an input field, and takes a min length as a prop - it displays a warning saying "too short" until the chars in the input meet the min length

---
+++ MinimumLength.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class MinimumLength extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			value: "",
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			value: e.currentTarget.value,
+		});
+
+	}
+
+	render() {
+		let { minLength } = this.props;
+		let { value } = this.state;
+
+		return (
+			<form>
+				<div className="form-group">
+					<h4>Give me words</h4>
+					<input
+						onChange={ this.handleChange }
+						value={ value }
+						className="form-control"
+					/>
+					{ value.length >= minLength ? null : <p className="text-danger form-text">Too short!</p> }
+				</div>
+			</form>
+		);
+	}
+}
+
+export default MinimumLength;
