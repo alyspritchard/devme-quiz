f8ed230e260b6bf59997dd424a167c6d1cae090b
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-06 11:01:31 +0100

    created a CountBy component that takes a step prop (numerical), and everytime you click the button the number increases by the step given

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Multiplier from './Multiplier';
 import EvenClicks from './EvenClicks';
+import CountBy from './CountBy';
 
 
 let App = () => (
@@ -9,6 +10,7 @@ let App = () => (
     <h1>Quizzzz</h1>
     <Multiplier x={ 10 } y={ 7 } />
     <EvenClicks />
+    <CountBy step={ 5 } />
   </>
 );
 
---
+++ CountBy.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class CountBy extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			count: 0,
+			
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		let current = this.state.count;
+
+		this.setState({
+			count: current + this.props.step,
+		});
+
+	}
+
+	render() {
+		let { step } = this.props;
+		let { count } = this.state;
+		
+		return (
+			<div className="card">
+				<p className="card-body">
+				{ count }
+				</p>
+				<button onClick={ this.handleClick } className="btn btn-danger">Clicker</button>
+			</div>
+		);
+	}
+}
+
+export default CountBy;
