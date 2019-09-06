04c09bd53e3ee476b8ce7c6fa289f8891dcdd4f9
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-06 12:16:52 +0100

    added separate routes for each challenge

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Multiplier from './Multiplier';
 import EvenClicks from './EvenClicks';
 import CountBy from './CountBy';
+import HideMe from './HideMe';
+import MinimumLength from './MinimumLength';
 
 
 let App = () => (
-  <>
-    <h1>Quizzzz</h1>
-    <Multiplier x={ 10 } y={ 7 } />
-    <EvenClicks />
-    <CountBy step={ 5 } />
-  </>
+	<Router>
+		<React.Fragment>
+			<h1>Quizzzz</h1>
+
+			<Route exact path="/multiplier" render={ () => (
+				<Multiplier x={ 10 } y={ 7 } /> 
+			) }/>
+
+			<Route exact path="/even-clicks" component={ EvenClicks } />
+
+			<Route exact path="/count-by" render={ () => (
+				<CountBy step={ 4 } /> 
+			) }/>
+
+			<Route exact path="/hide-me" render={ () => (
+				<HideMe>Boo!</HideMe> 
+			) }/>
+
+			<Route exact path="/minimum-length" render={ () => (
+				<MinimumLength minLength={ 20 } />
+			) }/>			
+
+		</React.Fragment>
+	</Router>
 );
 
 export default App;
