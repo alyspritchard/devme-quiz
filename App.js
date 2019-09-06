ddd8dd20581ccea2636604ed1fc980fc00c93f0b
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-06 10:42:36 +0100

    created a Multiplier component that takes 2 props (numbers) and outputs the product of them

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Multiplier from './Multiplier';
+
 
 let App = () => (
+  <>
+    <h1>Quizzzz</h1>
+    <Multiplier x={ 10 } y={ 7 } />
+  </>
 );
 
 export default App;
---
+++ Multiplier.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let Header = ({ x, y }) => (
+	<div class="card">
+		<p class="card-body">
+			{ x * y }
+		</p>
+	</div>
+); 
+
+
+export default Header;
